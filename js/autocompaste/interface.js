25e65cb2d189a0045c8eb17a35c571edca562a81
Joanne <joannemah.j.w@gmail.com>
2015-09-03 13:41:35 +0800

    Made some changes to instructions in experiment page

--- js/autocompaste/interface.js
+++ js/autocompaste/interface.js
@@ -140,7 +140,7 @@ AutoComPaste.Interface = (function () {
         privates.wm.createWindow("text_editor");
         privates.wm.setWindowTitle("text_editor", "Text Input Box");
         privates.wm.setWindowContent("text_editor", acp_textarea);
-        privates.wm.setWindowFooter("text_editor", "Press <b>Enter</b> once you are done.");
+        privates.wm.setWindowFooter("text_editor", "Hit <b>Enter</b> once you are done.");
         acp_textarea.focus();
 
         // Dispatch an event.
